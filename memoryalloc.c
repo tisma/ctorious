d5807e241e454cb9e69e4fa3c6d4146bd4aa88ef
Miroslav Tisma <tisma@linux.com>
2018-10-22 00:04:05 +0200

    Implement malloc, realloc, calloc funcitons.
    Compile with: gcc -shared -fPIC -o memoryalloc.so
    Run with: export LD_PRELOAD=$PWD/memoryalloc.so

--- memoryalloc.c
+++ memoryalloc.c
@@ -66,4 +66,100 @@ void free(void* block)
 void* malloc(size_t size)
 {
 	size_t total_size;
+	void *block;
+	struct header_t* header;
+
+	if (!size) {
+		return NULL;
+	}
+
+	pthread_mutex_lock(&global_malloc_lock);
+
+	header = get_free_block(size);
+
+	if (header) {
+		header->is_free = 0;
+		
+		pthread_mutex_unlock(&global_malloc_lock);
+
+		return (void*)(header + 1);
+	}
+
+	total_size = sizeof(struct header_t) + size;
+	block = sbrk(total_size);
+
+	if (block == (void*) -1) {
+		pthread_mutex_unlock(&global_malloc_lock);
+		return NULL;
+	}
+
+	header = block;
+	header->size = size;
+	header->is_free = 0;
+	header->next = NULL;
+
+	if (!head) {
+		head = header;
+	}
+	if (tail) {
+		tail->next = header;
+	}
+	tail = header;
+
+	pthread_mutex_unlock(&global_malloc_lock);
+
+	return (void*)(header + 1);
+}
+
+void* calloc(size_t num, size_t nsize)
+{
+	size_t size;
+	void* block;
+
+	if (!num || !nsize) {
+		return NULL;
+	}
+
+	size = num * nsize;
+
+	if (nsize != size / num) {
+		return NULL;
+	}
+
+	block = malloc(size);
+
+	if (!block) {
+		return NULL;
+	}
+
+	memset(block, 0, size);
+
+	return block;
+}
+
+void* realloc(void* block, size_t size)
+{
+	struct header_t* header;
+	void* ret;
+
+	if (!block || !size) {
+		return malloc(size);
+	}
+
+	header = (struct header_t*)block - 1;
+
+	if (header->size >= size) {
+		return block;
+	}
+
+	ret = malloc(size);
+
+	if (ret) {
+		memcpy(ret, block, header->size);
+
+		free(block);
+	}
+
+	return ret;
+}
 
